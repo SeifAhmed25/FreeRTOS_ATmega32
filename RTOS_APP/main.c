5ac2dbee80879a76871c796269483f6b84aba607
Seif <seif7760@gmail.com>
2024-04-21 18:42:17 +0200

    QUEUE Implemented with comments

--- RTOS_APP/main.c
+++ RTOS_APP/main.c
@@ -1,71 +1,86 @@
 #ifndef F_CPU
-
-#define F_CPU 16000000UL // clock speed is 16MHz
-
+#define F_CPU 16000000UL // Clock speed is 16MHz
 #endif
+
 #include "uart.h"
 #include "LCD.h"
 #include "board.h"
 #include "Keypad.h"
 #include "ADC.h"
-#include <util/delay.h> 
-/********************************		FreeRTOS Files Inclusion	**********************************/
-#include "FreeRTOS.h" 
+#include <util/delay.h>
+
+/******************************** FreeRTOS Files Inclusion **********************************/
+#include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
 #include "semphr.h"
 #include "event_groups.h"
 
-void system_init(void){
-	Leds_AllInit();
-	LCD_Init();
-	LCD_DispStrXY(1,1,(u8*)"Res App: ");
-	Key_Init();
-	Uart_Init(9600);
-	Uart_SendStr("Res App: ");
-}
+// Function prototypes
+void system_init(void);
 void T_T1(void* pvParam);
 void T_T2(void* pvParam);
 void T_T3(void* pvParam);
 
-QueueHandle_t mqUart = NULL; 
+
+QueueHandle_t mqUart = NULL;	/*Declare a variable of Queue Type and initialize with null*/
+#define MaxQueueLength		5
+#define MaxItemSize			11  /* In Bytes */
+
 int main(void)
 {
 	system_init();
-	mqUart = xQueueCreate(5, 11);  /*Queue Length and Max item size*/
+	mqUart = xQueueCreate(MaxQueueLength,MaxItemSize);
 	xTaskCreate(T_T1, NULL, 100, NULL, 1, NULL);
-	xTaskCreate(T_T2, NULL, 100, NULL, 2, NULL); 
+	xTaskCreate(T_T2, NULL, 100, NULL, 2, NULL);
 	vTaskStartScheduler();
 	return 0;
 }
-void T_T1(void* pvParam){
-	u8 u8Data = 0; 
-	u8 u8Ind = 0;
-	u8 txMsg[11]; 
-	while (1){
-		if (Uart_ReceiveByte_unblock(&u8Data)){ /*End of Receiving*/
-			if (u8Data == 0){
-				txMsg[u8Ind] = 0;  
-				u8Ind = 0; 
-				xQueueSend (mqUart, txMsg, portMAX_DELAY); 
+
+void system_init(void)
+{
+	Leds_AllInit();
+	LCD_Init();
+	LCD_DispStrXY(1, 1, (u8*)"Res App: ");
+	Key_Init();
+	Uart_Init(9600);
+	Uart_SendStr("Res App: ");
+}
+
+/*****************************		Task to recieve string and push it to queue		*************************/
+void T_T1(void* pvParam)
+{
+	u8 u8Data = 0; /* receive data in from UART data register */
+	u8 u8Ind = 0;  /* keep track of array index */
+	u8 txMsg[11];  /* array to store receiving message */
+	while (1)
+	{
+		if (Uart_ReceiveByte_unblock(&u8Data)) 
+		{
+			if (u8Data == NULL)/* Receiving a Null Data Represents the end of Receiving */ 
+			{	
+				txMsg[u8Ind] = 0;
+				u8Ind = 0;
+				xQueueSend(mqUart, txMsg, portMAX_DELAY);
 			}
-			else{ /*Still receiving bytes*/
-				txMsg [u8Ind] = u8Data; 
-				u8Ind++;	
+			else /* Still receiving data */
+			{
+				txMsg[u8Ind] = u8Data;
+				u8Ind++;
 			}
 		}
 	}
 }
-void T_T2(void* pvParam){ 
-	u8 rxMsg[11]; 
-	while (1){ 
-		if (xQueueReceive(mqUart, rxMsg, portMAX_DELAY)){
+
+/******************		Task to Read Data from Queue and send it by UART	***************/
+void T_T2(void* pvParam)
+{
+	u8 rxMsg[11]; /* Array to store data from Queue */
+	while (1)
+	{
+		if (xQueueReceive(mqUart, rxMsg, portMAX_DELAY))
+		{
 			Uart_SendStr(rxMsg);
 		}
-	}
-}
-void T_T3(void* pvParam){
-	while (1){
-
 	}
 }
